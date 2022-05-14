807905443ea9b6ba988421010d5c4d5e1a0ebd59
Elia-Tomaselli <elia.tomaselli@studenti.unitn.it>
2022-05-14 15:54:41 +0200

    Aggiunto inizio frontend con React

---
+++ Frontend/omi-finder/src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.scss";
+
+function App() {
+	return (
+		<div className={styles.App}>
+			<p>Hello world</p>
+		</div>
+	);
+}
+
+export default App;
