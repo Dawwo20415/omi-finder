9ca8bfd88a5afa8da49cd0864e6835dbf2ddfb12
Dawwo-15 <davidemongera@hotmail.com>
2022-05-19 16:56:33 +0200

    API request for zone Created
    Now the site can connect to database and expose api access for specific functions, with no argument yet

--- Software/BackEnd/atlas-interface.js
+++ Software/BackEnd/atlas-interface.js
@@ -1,60 +1,73 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
+//main().catch(err => console.log(err));
 
+/*
 async function main() {
 
     const zona = dbConnectionSetup();
 
-    console.log(await query(zona, {provincia:"VR"}, '-coordinate'));
-
-    console.log(await query(zona, {provincia:"VRT"}, '-coordinate'));
+    console.log(await api_test_function(zona));
 
     mongoose.disconnect();
 
     process.exit();
 }
+*/
 
-function dbConnectionSetup () {
-    const uri = 'mongodb+srv://' + process.env.USER + ':' + process.env.PASSWORD + '@omifinder.brexx.mongodb.net/OmiFinder?retryWrites=true&w=majority'; 
-    mongoose.connect(uri, () => {
-        console.log("Connecting...");
-    });
-
-    const omi_zone_schema = new mongoose.Schema({
-        provincia : String,
-        codice_catasto : String,
-        comune : String,
-        codice_zona : String,
-        linkzona : String,
-        valori : [{
-            semestre : String,
-            residenziale : {
-                abitazioni_civili : {min : String, max : String},
-                abitazioni_di_tipo_economico : {min : String, max : String},
-                ville_e_villini : {min : String, max : String}
-            },
-            commerciale : {
-                negozi : {min : String, max : String}
-            },
-            terziaria : {
-                uffici : {min : String, max : String}
-            }
-        }],
-        coordinate : [ String ]
-    }, {collection : 'ZoneOmi'});
-
-    const Zona = mongoose.model('ZoneOmi', omi_zone_schema);
-
-    return Zona;
-}
-
-
-async function query(model, filter, projection) {
+async function query (model, filter, projection) {
 
     //Execute Query
     result = await model.find(filter, projection);
 
     return result;
 }
+
+module.exports = {
+    dbConnectionSetup : function () {
+        const uri = 'mongodb+srv://' + process.env.USER + ':' + process.env.PASSWORD + '@omifinder.brexx.mongodb.net/OmiFinder?retryWrites=true&w=majority'; 
+        mongoose.connect(uri, () => {
+            console.log("Connecting...");
+        });
+    
+        const omi_zone_schema = new mongoose.Schema({
+            provincia : String,
+            codice_catasto : String,
+            comune : String,
+            codice_zona : String,
+            linkzona : String,
+            valori : [{
+                semestre : String,
+                residenziale : {
+                    abitazioni_civili : {min : String, max : String},
+                    abitazioni_di_tipo_economico : {min : String, max : String},
+                    ville_e_villini : {min : String, max : String}
+                },
+                commerciale : {
+                    negozi : {min : String, max : String}
+                },
+                terziaria : {
+                    uffici : {min : String, max : String}
+                }
+            }],
+            coordinate : [ String ]
+        }, {collection : 'ZoneOmi'});
+    
+        const Zona = mongoose.model('ZoneOmi', omi_zone_schema);
+    
+        return Zona;
+    },
+
+    api_test_function : async function (model) {
+        const result = await query(model, {provincia:"VR"}, '-coordinate');
+    
+        return result;
+    }
+}
+
+
+
+
+
+
--- Software/FrontEnd/start.js
+++ Software/FrontEnd/start.js
@@ -1,5 +1,6 @@
 //Packages
 const express = require('express')
+const custom_api = require('../BackEnd/atlas-interface.js')
 
 //Definisce la porta su cui ascoltare | Heroku RICHIEDE la porta 5000
 const PORT = process.env.PORT || 5000
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 5000
 //Definisce una istanza della "classe" express con cui si interagisce
 //con l'interfaccia web
 const web_interface = express()
+
+const zona = custom_api.dbConnectionSetup()
 
 //Fare cose con l'interfaccia web
 
@@ -18,6 +21,12 @@ web_interface.get( "/", (req, res) => {
 
 web_interface.get( "/testpage", (req, res) => {
     res.send("<h1>Atlas Test Page</h1>")
+})
+
+web_interface.get( "/v1/testcommand", async (req, res) => {
+    const result = await custom_api.api_test_function(zona)
+    console.log(result)
+    res.json(result)
 })
 
 //Dire ad espress su quale porta ascoltare le richieste
